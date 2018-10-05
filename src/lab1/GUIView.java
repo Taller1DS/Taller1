99ac7fdc61316398180a986abb4f1fa3d2c02c4f
Axel Jeremy Godoy Saavedra <ajgodoy@espol.edu.ec>
2018-10-04 22:01:34 -0500

    Cambios integrante 2

--- src/lab1/GUIView.java
+++ src/lab1/GUIView.java
@@ -50,7 +50,7 @@ public class GUIView extends JPanel {
 		this.gameFactory = factory;
 
 		// Set the background on the GameView
-		this.gameView.setBackground(Color.white);
+		this.gameView.setBackground(Color.LIGHT_GRAY);
 
 		// Set the layout on myself
 		setLayout(new BorderLayout());
@@ -63,7 +63,7 @@ public class GUIView extends JPanel {
 
 		// Create a new button on that panel and add a StartGameListener as
 		// listener on that button
-		this.startGameButton = new JButton("Start Game");
+		this.startGameButton = new JButton("Let’s Play");
 		this.startGameButton.addActionListener(new StartGameListener());
 		this.guiPanel.add(this.startGameButton);
 
@@ -101,7 +101,7 @@ public class GUIView extends JPanel {
 						GUIView.this.gameChooser.getSelectedItem().toString();
 				GameModel gameModel =
 						GUIView.this.gameFactory.createGame(gameName);
-
+                                GUIView.this.gameView.setBackground(Color.WHITE);
 				// Stop current game (if any) and start a new game with the
 				// new game model
 				GUIView.this.gameController.stopGame();
